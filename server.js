4f857667001f6710c5c60fe7ad34de0691528cca
brendanjlewis <brendan.j.f.lewis@gmail.com>
2021-12-29 22:19:03 -0500

    htmlroutes working, commented out apiroutes

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 const app = express();
 const htmlRoutes = require('./routes/htmlRoutes');
+// const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));;
 
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
