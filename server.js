5ccec47cf8fa63908a320882c696b2801e2c7bf4
brendanjlewis <brendan.j.f.lewis@gmail.com>
2021-12-29 21:41:47 -0500

    added routes

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));;
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+    console.log(`API server now on port ${PORT}`);
 });
