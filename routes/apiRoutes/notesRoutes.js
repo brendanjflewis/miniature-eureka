b0a9fee832c12a6fe998e0124a245e91815e6ce1
brendanjlewis <brendan.j.f.lewis@gmail.com>
2021-12-30 20:49:11 -0500

    npm package uuid installed and functional

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,26 +1,38 @@
 const router = require('express').Router();
-const { createNewNote, findById } = require('../../lib/notes');
+const { createNewNote } = require('../../lib/notes');
 let notes = require('../../db/db.json');
+const uuid = require('uuid');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
 });
 
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, aniamls);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
-});
-
+//integrated uuid npm pacakge and reworked library/notesRoutes files to make id work
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+    note = {
+        id: uuid.v4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    createNewNote(note, notes);
+    res.json(notes);
 });
 
 module.exports = router;
 
+
+// router.get('/notes/:id', (req, res) => {
+//     const result = findById(req.params.id, aniamls);
+//     if (result) {
+//         res.json(result);
+//     } else {
+//         res.send(404);
+//     }
+// });
+
+// router.post('/notes', (req, res) => {
+//     req.body.id = notes.length.toString();
+//     const note = createNewNote(req.body, notes);
+//     res.json(note);
+// });
