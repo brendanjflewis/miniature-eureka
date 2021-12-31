ea0faf5b50790023347bc2ca334c90e7092b8cfe
brendanjlewis <brendan.j.f.lewis@gmail.com>
2021-12-30 19:16:43 -0500

    added working api routes, connected front to back

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require('express');
 const PORT = process.env.PORT || 3005;
 const app = express();
 const htmlRoutes = require('./routes/htmlRoutes');
-// const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));;
+app.use(express.static('public'));
 
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
